776d7cc0058d3e7c54d9bc652e7d0ecf65cb380e
Julian-Lop <julian01andres@gmail.com>
2023-10-10 15:49:13 -0500

    fonts - icons download and colors set

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        
+        <h1>
+          Lato
+        </h1>
       </main>
     </>
   )
