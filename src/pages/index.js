cf8d0443b17a98301459a512201eba2077aec5e9
Julian-Lop <julian01andres@gmail.com>
2023-10-21 22:40:19 -0500

    project section maquetacion 60%

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
+//Components
 import Cards from "@/components/Cards";
 import Form from "@/components/Form";
 import Section from "@/components/Section";
 import Button from "@/components/buttons/Button";
 import Layout from "@/components/layouts/Layout";
+
+//Images
+import imagen1 from '../../public/images/Atardecer.jpg'
+import imagen2 from '../../public/images/Bosque.png'
+import imagen3 from '../../public/images/Samurai.jpg'
 
 
 export default function Home() {
@@ -54,11 +61,78 @@ export default function Home() {
             <Cards/>
           </div>
         </Section>
-        <Section classes={'Three'} id={'Projects'} >
+        <Section classes={'Three noreverse'} id={'Projects'} >
           <h2>Proyectos <span>{'</>'}</span></h2>
           <div className="Content">
+            {/* Contenedor de imagenes */}
+            <div className="Accordion_img">
+              <Image src={imagen1} alt=""  className="Accord_img active" />
+              <Image src={imagen2} alt=""  className="Accord_img" />
+              <Image src={imagen3} alt=""  className="Accord_img" />
+            </div>
             
+            {/* Acordeon */}
+            <div className="Accordion">
+
+              {/* Lista de acordeon */}
+              <ul className="Accordion_list">
+
+                {/* Item1 */}
+                <li className="Accordion_item active">
+                  <h3 className="Accordion_header">
+                    <span className="Accordion_bl">
+                      <span className="Accordion_bl_line"></span>
+                      <span className="Accordion_bl_line"></span>
+                    </span>
+                    <span className="Accordion_title">Pagina Web Emilan</span>
+                  </h3>
+                  <div className="Accordion_body">
+                    <p className="Accordion_descript">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque,
+                      metus malesuada.
+                    </p>
+                  </div>
+                </li>
+
+                {/* Item2 */}
+                <li className="Accordion_item">
+                  <h3 className="Accordion_header">
+                    <span className="Accordion_bl">
+                      <span className="Accordion_bl_line"></span>
+                      <span className="Accordion_bl_line"></span>
+                    </span>
+                    <span className="Accordion_title">Pagina Web 2</span>
+                  </h3>
+                  <div className="Accordion_body">
+                    <p className="Accordion_descript">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque,
+                      metus malesuada.
+                    </p>
+                  </div>
+                </li>
+
+                {/* Item3 */}
+                <li className="Accordion_item">
+                  <h3 className="Accordion_header">
+                    <span className="Accordion_bl">
+                      <span className="Accordion_bl_line"></span>
+                      <span className="Accordion_bl_line"></span>
+                    </span>
+                    <span className="Accordion_title">Pagina Web 3</span>
+                  </h3>
+                  <div className="Accordion_body">
+                    <p className="Accordion_descript">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque,
+                      metus malesuada.
+                    </p>
+                  </div>
+                </li>
+
+              </ul>      
+            </div>
+
           </div>
+
         </Section>
         <Section classes={'Three noreverse'} id={'Contact'} >
           <h2 style={{textAlign: 'right'}}><span>{'<?>'}</span> Contáctame</h2>
