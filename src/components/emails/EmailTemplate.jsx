e18632775e758e53dfc0a737c368c40341d78231
Julian-Lop <julian01andres@gmail.com>
2023-11-05 17:04:42 -0500

    remove .env git

--- src/components/emails/EmailTemplate.jsx
+++ src/components/emails/EmailTemplate.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function EmailTemplate({name,email,message}) {
   return (
-    <div style={{ background: 'blue', borderRadius: '20px'}}>
-      <h2>De: {name}</h2>
-      <hr />
-      <strong>{email}</strong>
+    <div style={{ background:'#f2f2f2', borderRadius: '10px', padding: '25px'}}>
+      <h2 style={{color: '#8093ff'}}>De: {name}</h2>
+      
+      <hr style={{background: '#313F8c', height: '4px'}} />
+      
+      <strong style={{color: '#242e66'}}>{email}</strong>
 
-      <p>{message}</p>
+      <p style={{color: '#242e66cc'}}>{message}</p>
     </div>
   )
 }
