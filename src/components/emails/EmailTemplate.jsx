ea4ee9832c68bcc6bb0bc2ae9ec1929cb304de27
Julian-Lop <julian01andres@gmail.com>
2023-11-05 17:10:01 -0500

    finished style email-template

--- src/components/emails/EmailTemplate.jsx
+++ src/components/emails/EmailTemplate.jsx
@@ -5,11 +5,9 @@ export default function EmailTemplate({name,email,message}) {
     <div style={{ background:'#f2f2f2', borderRadius: '10px', padding: '25px'}}>
       <h2 style={{color: '#8093ff'}}>De: {name}</h2>
       
-      <hr style={{background: '#313F8c', height: '4px'}} />
-      
       <strong style={{color: '#242e66'}}>{email}</strong>
 
-      <p style={{color: '#242e66cc'}}>{message}</p>
+      <p style={{color: '#242e66cc', fontWeight: '600px'}}>{message}</p>
     </div>
   )
 }
